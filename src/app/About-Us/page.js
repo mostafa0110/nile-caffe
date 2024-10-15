99c7f02a000bf99c2b089af43a7b29c910864010
lazermagma <masterabdelrahman7@gmail.com>
2024-10-15 09:03:20 +0300

    Fix : About us responsiveness

--- src/app/About-Us/page.js
+++ src/app/About-Us/page.js
@@ -2,16 +2,16 @@ import React from "react";
 
 const AboutUs = () => {
   return (
-    <div className="h-full w-full  bg-zinc-900 flex flex-col">
+    <div className="h-full w-full bg-zinc-900 flex flex-col">
       <div className="text-center mb-8 pt-40">
         <p className="text-2xl text-green-500 mb-4">About Us</p>
         <div className="h-0.5 w-10 bg-green-500 mx-auto"></div>
       </div>
-      <div className="md:pt-20 flex md:flex-row flex-col justify-evenly">
-        <div className="text-white md:p-8 p-6 ">
-          <h2 className=" font-extrabold text-2xl">About Nile Café</h2>
+      <div className="md:pt-20 flex flex-col md:flex-row justify-center gap-11">
+        <div className="text-white md:p-8 p-6">
+          <h2 className="font-extrabold text-2xl">About Nile Café</h2>
           <br />
-          <p className="w-[31.25rem]">
+          <p className="text-base md:text-2xl md:w-[31.25rem]">
             Welcome to Nile Café, your all-in-one management solution designed
             specifically for restaurants and cafés. We understand the fast-paced
             nature of the food and beverage industry and have crafted a system
@@ -30,10 +30,11 @@ const AboutUs = () => {
             restaurant or café to the next level!
           </p>
         </div>
-        <div className="text-white md:p-8 p-6 ">
+        <div className="text-white md:p-8 p-6 flex justify-center">
           <img
-            src="/assets/images/AboutUs.jpg "
-            className="w-[31.25rem] h-[31.25rem]"
+            src="/assets/images/AboutUs.jpg"
+            alt="About Us"
+            className="w-full max-w-[31.25rem] h-auto rounded-2xl" // Adjusted for responsiveness
           />
         </div>
       </div>
